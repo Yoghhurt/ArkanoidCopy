ae3798a07f9c063d289503025a2fef8b65edd066
JackBillingham <billingham.jacks@gmail.com>
2025-01-21 14:03:38 +0100

    Added movements and resets + start on main

--- main.c
+++ main.c
@@ -14,8 +14,8 @@ typedef struct Ball {
 
 #define MAX_BRICKS 100
 typedef struct Brick {
-    Rectangle rect:
-    bool active;
+    Rectangle rect;
+    _Bool active;
 } Brick;
 
 static void UpdatePaddle(Paddle *paddle) {
@@ -24,5 +24,59 @@ static void UpdatePaddle(Paddle *paddle) {
     }
     if (IsKeyDown(KEY_RIGHT) && paddle->position.x < GetScreenWidth() - paddle->width) {
         paddle->position.x += 5.0f;
+    }
+}
+
+static void UpdateBall(Ball *ball, Paddle paddle, Brick bricks[], int *score, int *lives) {
+    ball->position.x += ball->speed.x;
+    ball->position.y += ball->speed.y;
+
+    if (ball->position.x < 0 || ball->position.x > GetScreenWidth()) {
+        ball->speed.x *= -1;
+    }
+    if (ball->position.y < 0) {
+        ball->speed.y *= -1;
+    } else if (ball->position.y > GetScreenHeight()) {
+        (*lives)--;
+        ball->position = (Vector2){GetScreenWidth() / 2, GetScreenHeight() / 2};
+        ball->speed = (Vector2){200.0f, 200.0f};
+        if (*lives <= 0) {
+            ball->speed = (Vector2){0, 0};
+        }
+    }
+
+    if (CheckCollisionCircles(ball->position, ball->radius, (Vector2){
+    paddle.position.x + paddle.width / 2, paddle.position.y}, paddle.height)) {
+        ball->speed.y *= -1;
+    }
+
+    for (int i = 0; i < MAX_BRICKS; i++) {
+        if (bricks[i].active && CheckCollisionCircleRec(ball->position, ball->radius, bricks[i].rect)) {
+            ball->speed.y *= -1;
+            bricks[i].active = false;
+            (*score) += 10;
+        }
+    }
+}
+
+int main() {
+    InitWindow(800, 600, "ArkanoidCopy");
+
+
+    int score = 0;
+    int lives = 3;
+    bool gameOver = false;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        if (!gameOver) {
+            UpdatePaddle(&paddle);
+            UpdateBall(&ball, paddle, bricks, &score, &lives);
+
+            if (lives <= 0) {
+                gameOver = true;
+            }
+        }
     }
 }
